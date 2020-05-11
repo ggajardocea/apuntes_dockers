3684958b97028447184925b05cdca956fdb352c8
ggajardo <ggajardo@entel.cl>
2020-05-11 03:07:18 +0000

    se guardar el dockerfile del shiny que funciona

---
+++ ejemplos/Docker_ShinyApp/app/global.R
@@ -0,0 +1,10 @@
+library(dplyr)
+library(data.table)
+data <- fread("data.txt", dec=",") %>% 
+  mutate_all(as.numeric)
+
+# Compute more values from x variable in a helper dataframe
+data_helper <- NULL
+data_helper$sqrt_x <- sqrt(data$x)
+data_helper$x2 <- (data$x)^2
+data_helper$x3 <- (data$x)^3
---
+++ ejemplos/ej_wena_perro/src/index.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "Wena culiao";
